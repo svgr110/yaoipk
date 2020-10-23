623e2513fbe5f9996fac262296132357a903f490
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-10-23 10:13:01 +0800

    Update diy-1.sh

--- Lede/diy-1.sh
+++ Lede/diy-1.sh
@@ -17,8 +17,8 @@ git clone https://github.com/jerrykuku/node-request.git package/lean/node-reques
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
-
-git clone https://github.com/wc5bk6/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
